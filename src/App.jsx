97bcd19dbe37702c0250f560b72c5c723d964b6d
Faheem Ul Hassan Aziz <seebiz.gpt0623.dev56@gmail.com>
2024-03-20 14:36:38 +0500

    firebase setup is ok and frontend is made

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,43 @@
 import { useState } from "react";
 
+import {
+  addDoc,
+  collection,
+  db,
+  //   getDocs,
+  query,
+  onSnapshot,
+  doc,
+  updateDoc,
+  deleteDoc,
+} from "./firebase.confog";
+
 function App() {
-  return <></>;
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+
+  const createuser = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <form onSubmit={createuser}>
+        <input
+          type="text"
+          value={name}
+          placeholder="Enter your Name"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="number"
+          placeholder="Enter your Age"
+          value={age}
+          onChange={(e) => setAge(e.target.value)}
+        />
+      </form>
+    </>
+  );
 }
 
 export default App;
